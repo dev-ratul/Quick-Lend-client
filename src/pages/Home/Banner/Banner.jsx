64f22d2cdbee610b83de3d0e54da2a695b2824fb
syz-bash <syzz.bd@gmail.com>
2025-09-08 01:43:30 +0600

    banner complete

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,31 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-2 items-center justify-between px-10 py-16 bg-primary">
-      {/* Left side (Text + Button) */}
-      <div className="max-w-lg gri">
-        <p className='bg-[#DBCBB91A] p-2  text-white'>
+    <div className="bg-primary w-full">
+      <div className="flex items-center justify-between px-10 py-16 ">
+        {/* Left side  */}
+        <div className="flex-1 ">
+          <p className="bg-[#DBCBB91A] p-2 text-[14px] px-4 rounded-full w-fit text-white">
             Trusted by 10,000+ clients and lenders
-        </p>
-        <h1 className="text-4xl font-bold mb-4 text-white">
-          Revolutionizing <span className='text-[#DBCBB9]'>Lending Solutions</span>
-        </h1>
-        <p className="text-gray-600 mb-6">
-          Get fast, fair, and transparent loans from trusted lenders.
-        </p>
-        <button className="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-blue-700 transition">
-          Get Started
-        </button>
-      </div>
+          </p>
+          <h1 className="text-5xl my-5 font-bold mb-4 text-white">
+            Revolutionizing{" "}
+            <span className="text-[#DBCBB9]">Lending Solutions</span>
+          </h1>
+          <p className="text-[#FBFAF8] mb-6">
+            Our platform helps clients get fair credit ratings and connects them
+            with trusted lenders for faster, more transparent lending decisions
+            across multiple industries.
+          </p>
+          <button className="bg-[#DBCBB9] text-white  rounded-lg text-[14px]  transition">
+            Get Started as Client
+          </button>
+        </div>
 
-      {/* Right side (Image) */}
-      <div>
-        <img
-          src="https://i.ibb.co.com/Xff5FMRw/d0b422cf28705267493b2da874b4d910bcb2ef9f.jpg"
-          alt="banner"
-          className="rounded-lg shadow-lg"
-        />
+        {/* Right side img */}
+        <div className="flex-1 flex justify-center">
+          <img
+            src="https://i.ibb.co.com/Xff5FMRw/d0b422cf28705267493b2da874b4d910bcb2ef9f.jpg"
+            alt="banner"
+            className="rounded-lg shadow-lg max-w-full"
+          />
+        </div>
       </div>
     </div>
   );
